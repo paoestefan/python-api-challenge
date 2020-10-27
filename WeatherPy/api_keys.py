c3e0195df214efa6ef707017e6eb77973f1a2570
unknown <paoestefan@hotmail.com>
2020-10-26 20:22:03 -0600

    First try of solving the challenge

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e9a97759515fb29e4f26c256e7ad31c6"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCk-CwVGYFNc3ZdYkYkqSc6JfOxM06dQ64"
