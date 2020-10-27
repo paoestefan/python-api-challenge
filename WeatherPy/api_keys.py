1a8f13e87331005da00100e06e4caae12415d93a
unknown <paoestefan@hotmail.com>
2020-10-27 14:13:26 -0600

    Upload of the first part finished, now working with the VacationPy file

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "e9a97759515fb29e4f26c256e7ad31c6"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyCk-CwVGYFNc3ZdYkYkqSc6JfOxM06dQ64"
